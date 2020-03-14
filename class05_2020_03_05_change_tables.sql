8996098355e7a54d9bdd1e55d29e70827fbb9eeb
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-03-13 23:25:09 -0300

    Adding class05

---
+++ class05_2020_03_05_change_tables.sql
@@ -0,0 +1,5 @@
+ALTER TABLE - altera uma tabela do banco de dados
+
+ADD - adiciona um novo atributo ou constraint
+DROP - excluir um atributo ou constraint
+MODIFY - modificar um atributo (tipo de dado ou tamanho)
