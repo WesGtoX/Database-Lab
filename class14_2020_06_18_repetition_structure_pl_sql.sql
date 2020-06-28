3a0f9da5b241c0bd71a9dfe8e81a174c082d9cbf
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-06-27 23:43:55 -0300

    Adding class14

---
+++ class14_2020_06_18_repetition_structure_pl_sql.sql
@@ -0,0 +1,85 @@
+-- ESTRUTURA DE REPETIÇÃO PL/SQL
+
+-- Estrutura de Reoetição FOR
+
+-- Sintaxe:
+FOR controle IN initial_value .. final_value
+LOOP
+    -- instruções PL/SQL
+END LOOP;
+
+-- Exemplo:
+DECLARE
+vcontrole NUMBER(2);
+BEGIN
+vcontrole := 11;
+FOR vcontrole IN 11 .. 20
+    LOOP
+    IF (vcontrole <= 15) THEN
+        INSERT INTO PRODUTO VALUES(vcontrole, 'Produto X' || vcontrole, 3, 10+vcontrole);
+    ELSE
+        INSERT INTO PRODUTO VALUES(vcontrole, 'Produto Z' || vcontrole, 5, 20+vcontrole);
+    END IF;
+END LOOP;
+END;
+/
+
+
+-- Estrutura de Reoetição WHILE
+
+-- Sintaxe:
+WHILE (condicao)
+LOOP
+    -- instruções PL/SQL
+END LOOP;
+
+-- Exemplo:
+DECLARE
+vcontrole NUMBER(2);
+BEGIN
+vcontrole := 15;
+WHILE(vcontrole <= 20)
+LOOP
+    UPDATE PRODUTO SET vlr_produto = vlr_produto * 1.05 WHERE id_produto = vcontrole;
+vcontrole := vcontrole+1;
+END LOOP;
+END;
+/
+
+
+-- PROCEDIMENTOS (PROCEDURE)
+
+-- Exemplo
+-- Retornar valor total de um orçamento a partir do seu número (id_orcamento)
+CREATE OR REPLACE PROCEDURE ptotal(vnumero IN ITEM_ORCAMENTO.id_orcamento%TYPE)
+IS  -- quando é necessário declarar variável.
+vtotal NUMBER(12,2);
+BEGIN
+SELECT SUM(qtd_orcamento * vlr_ITEM_ORCAMENTO) INTO vtotal 
+FROM ITEM_ORCAMENTO WHERE id_orcamento = vnumero;
+DBMS_OUTPUT.PUT_LINE('Total = R$' || vtotal);
+EXCEPTION
+WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE('Erro ou id_orcamento inexistente...');
+END;
+/
+
+exec ptotal(2)  -- executa um procedimento, passando o id.
+
+SET SERVEROUTPUT ON;  -- ativa visualização das variáveis.
+
+SHOW ERRORS;  -- mostrar os erro.
+
+
+-- Procedimento para cadastrar clientes
+CREATE OR REPLACE PROCEDURE cadcli(vnome IN CLIENTE.nom_cliente%TYPE, 
+    vdtnasc IN CLIENTE.dtn_cliente%TYPE, vemail IN CLIENTE.email_cliente%TYPE, vdoc IN CLIENTE.doc_cliente%TYPE)
+AS  -- quando não é necessário declarar variável.
+BEGIN
+INSERT INTO CLIENTE VALUES (seq_cod_cliente.NEXTVAL, vnome, vdtnasc, vemail, vdoc);
+COMMIT;
+EXCEPTION
+WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE('Erro na execução...');
+END;
+/
---
+++ class14_2020_06_25_functions_pl_sql.sql
@@ -0,0 +1,51 @@
+-- FUNCTIONS (FUNÇÕES) PL/SQL
+Programa PL/SQL que permite o retorno de informações
+
+-- Sintaxe
+CRETE [OR REPLACE] FUNCTION "function_name"
+(parâmetro tipo_do_parametro tipo_dado_parametro, ...)
+RETURN tipo_do_retorno
+IS ou AS
+-- Declaração das variáveis
+BEGIN
+-- Instruções PL/SQL
+RETURN o que será retornado
+END;
+
+
+
+
+-- Função para retornar o documento do cliente formatado
+CREATE OR REPLACE FUNCTION FORMATADOC(vdoc IN CLIENTE.doc_cliente%TYPE)
+RETURN VARCHAR2
+IS
+vdocformatado VARCHAR2(19);
+BEGIN
+IF (LENGTH(vdoc) = 11) THEN
+    vdocformatado := SUBSTR(vdoc, 1, 3) || '.' || SUBSTR(vdoc, 4, 3) || '.' || SUBSTR(vdoc, 7, 3) || '-' || SUBSTR(vdoc, 10);
+ELSIF (LENGTH(vdoc) = 14) THEN
+    vdocformatado := SUBSTR(vdoc, 1, 2) || '.' || SUBSTR(vdoc, 3, 3) || '.' || SUBSTR(vdoc, 5, 3) || '/' || SUBSTR(vdoc, 9, 4) || '.' || SUBSTR(vdoc, 13);
+ELSE
+    vdocformatado := vdoc;
+END IF;
+RETURN vdocformatado;
+END;
+/
+
+
+-- Testando a função FORMATADOC
+SELECT nom_cliente, FORMATADOC(doc_cliente) DOCUMENTO FROM CLIENTE;
+
+
+-- Função para retornar a idade do cliente
+CREATE OR REPLACE FUNCTION IDADE(vdtn IN CLIENTE.dtn_cliente%TYPE)
+RETURN NUMBER
+IS
+vidade NUMBER(3);
+BEGIN
+vidade := TRUNC(MONTHS_BETWEEN(SYSDATE, vdtn)/12);
+RETURN vidade;
+END;
+/
+
+SELECT nom_cliente, IDADE(dtn_cliente) IDADE FROM CLIENTE;
