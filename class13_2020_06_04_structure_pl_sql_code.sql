28fac3bd8738df0258e9005b30132a127bb7555d
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-07-03 00:03:45 -0300

    Adding class15

--- class13_2020_06_04_structure_pl_sql_code.sql
+++ class13_2020_06_04_structure_pl_sql_code.sql
@@ -41,11 +41,11 @@ SET SERVEROUTPUT ON;
 DECLARE
 vtaxa NUMBER(4,2);
 BEGIN
-vtaxa := &vtaxa;
-UPDATE PRODUTO SET vlr_produto = vlr_produto + vlr_produto * (vtaxa / 100);
+    vtaxa := &vtaxa;
+    UPDATE PRODUTO SET vlr_produto = vlr_produto + vlr_produto * (vtaxa / 100);
 EXCEPTION
 WHEN OTHERS THEN
-DBMS_OUTPUT.PUT_LINE('Erro na execução da atualização');
+    DBMS_OUTPUT.PUT_LINE('Erro na execução da atualização');
 END;
 /
 
@@ -53,23 +53,23 @@ END;
 -- Bloco anônimo para ler o código de um cliente e mostrar seu nome e sua data de nascimento.
 DECLARE
 -- Declaração das variáveis
-vcod_cliente NUMBER(6);
-vnom_cliente VARCHAR(30);
-vdtn_cliente DATE;
+    vcod_cliente NUMBER(6);
+    vnom_cliente VARCHAR(30);
+    vdtn_cliente DATE;
 BEGIN
--- Entrada de dados.
-vcod_cliente := &vcod_cliente;
--- Processamento.
-SELECT nom_cliente, dtn_cliente INTO vnom_cliente, vdtn_cliente
-FROM CLIENTE
-WHERE cod_cliente = vcod_cliente;
--- Saída de dados.
-DBMS_OUTPUT.PUT_LINE('Nome: ' || vnom_cliente);
-DBMS_OUTPUT.PUT_LINE('Data Nascimento: ' || vdtn_cliente);
+    -- Entrada de dados.
+    vcod_cliente := &vcod_cliente;
+    -- Processamento.
+    SELECT nom_cliente, dtn_cliente INTO vnom_cliente, vdtn_cliente
+    FROM CLIENTE
+    WHERE cod_cliente = vcod_cliente;
+    -- Saída de dados.
+    DBMS_OUTPUT.PUT_LINE('Nome: ' || vnom_cliente);
+    DBMS_OUTPUT.PUT_LINE('Data Nascimento: ' || vdtn_cliente);
 -- Tratamento de erros.
 EXCEPTION
 WHEN OTHERS THEN
-DBMS_OUTPUT.PUT_LINE('Erro na execução da atualização');
+    DBMS_OUTPUT.PUT_LINE('Erro na execução da atualização');
 END;
 /
 
@@ -77,11 +77,11 @@ END;
 -- ESTRUTURA CONDICIONAL IF ... ELSIF ... ELSE.
 -- Sintaxe:
 IF (condição) THEN
--- Instruções para condição verdadeira.
+    -- Instruções para condição verdadeira.
 ELSEIF (condição2) THEN
--- Instruções para condição2 verdadeira.
+    -- Instruções para condição2 verdadeira.
 ELSE
--- Instruções quando as condições anteriores forem falsas.
+    -- Instruções quando as condições anteriores forem falsas.
 END IF;
 
 
@@ -97,31 +97,31 @@ INSERT INTO CLIENTE VALUES (8, 'Cliente 8', '01/01/1950', 'client8@client.com', 
 
 -- Bloco anônimo para ler o código do cliente e mostrar o nome e a classificação (pessoa física ou pessoa jurídica).
 DECLARE
--- Declaração das variáveis
-vcod_cliente NUMBER(6);
-vnom_cliente VARCHAR2(30);
-vcat_cliente VARCHAR2(20);
-vdoc_cliente NUMBER(2);
+    -- Declaração das variáveis
+    vcod_cliente NUMBER(6);
+    vnom_cliente VARCHAR2(30);
+    vcat_cliente VARCHAR2(20);
+    vdoc_cliente NUMBER(2);
 -- Início do bloco PL/SQL
 BEGIN
--- Entrada de dados
-vcod_cliente := &vcod_cliente;
-SELECT nom_cliente, LENGTH(doc_cliente) 
-INTO vnom_cliente, vdoc_cliente FROM CLIENTE
-WHERE cod_cliente = vcod_cliente;
-IF(vdoc_cliente = 11) THEN
-    vcat_cliente := 'PESSOA FISICA';
-ELSIF (vdoc_cliente = 14) THEN
-    vcat_cliente := 'PESSOA JURIDICA';
-ELSE
-    vcat_cliente := 'DOCUMENTO INVALIDO';
-END IF;
--- Saída de dados
-DBMS_OUTPUT.PUT_LINE('Nome: ' || vnom_cliente);
-DBMS_OUTPUT.PUT_LINE('Categoria: ' || vcat_cliente);
--- Tratamento de erros
+    -- Entrada de dados
+    vcod_cliente := &vcod_cliente;
+    SELECT nom_cliente, LENGTH(doc_cliente) 
+    INTO vnom_cliente, vdoc_cliente FROM CLIENTE
+    WHERE cod_cliente = vcod_cliente;
+    IF(vdoc_cliente = 11) THEN
+        vcat_cliente := 'PESSOA FISICA';
+    ELSIF (vdoc_cliente = 14) THEN
+        vcat_cliente := 'PESSOA JURIDICA';
+    ELSE
+        vcat_cliente := 'DOCUMENTO INVALIDO';
+    END IF;
+    -- Saída de dados
+    DBMS_OUTPUT.PUT_LINE('Nome: ' || vnom_cliente);
+    DBMS_OUTPUT.PUT_LINE('Categoria: ' || vcat_cliente);
+    -- Tratamento de erros
 EXCEPTION
 WHEN OTHERS THEN
-DBMS_OUTPUT.PUT_LINE('Erro de execução ou cliente não cadastrado...');
+    DBMS_OUTPUT.PUT_LINE('Erro de execução ou cliente não cadastrado...');
 END;
 /
--- class15_2020_06_25_functions_pl_sql.sql
+++ class15_2020_06_25_functions_pl_sql.sql
@@ -6,9 +6,9 @@ CRETE [OR REPLACE] FUNCTION "function_name"
 (parâmetro tipo_do_parametro tipo_dado_parametro, ...)
 RETURN tipo_do_retorno
 IS ou AS
--- Declaração das variáveis
+    -- Declaração das variáveis
 BEGIN
--- Instruções PL/SQL
+    -- Instruções PL/SQL
 RETURN o que será retornado
 END;
 
@@ -19,15 +19,15 @@ END;
 CREATE OR REPLACE FUNCTION FORMATADOC(vdoc IN CLIENTE.doc_cliente%TYPE)
 RETURN VARCHAR2
 IS
-vdocformatado VARCHAR2(19);
+    vdocformatado VARCHAR2(19);
 BEGIN
-IF (LENGTH(vdoc) = 11) THEN
-    vdocformatado := SUBSTR(vdoc, 1, 3) || '.' || SUBSTR(vdoc, 4, 3) || '.' || SUBSTR(vdoc, 7, 3) || '-' || SUBSTR(vdoc, 10);
-ELSIF (LENGTH(vdoc) = 14) THEN
-    vdocformatado := SUBSTR(vdoc, 1, 2) || '.' || SUBSTR(vdoc, 3, 3) || '.' || SUBSTR(vdoc, 5, 3) || '/' || SUBSTR(vdoc, 9, 4) || '.' || SUBSTR(vdoc, 13);
-ELSE
-    vdocformatado := vdoc;
-END IF;
+    IF (LENGTH(vdoc) = 11) THEN
+        vdocformatado := SUBSTR(vdoc, 1, 3) || '.' || SUBSTR(vdoc, 4, 3) || '.' || SUBSTR(vdoc, 7, 3) || '-' || SUBSTR(vdoc, 10);
+    ELSIF (LENGTH(vdoc) = 14) THEN
+        vdocformatado := SUBSTR(vdoc, 1, 2) || '.' || SUBSTR(vdoc, 3, 3) || '.' || SUBSTR(vdoc, 5, 3) || '/' || SUBSTR(vdoc, 9, 4) || '.' || SUBSTR(vdoc, 13);
+    ELSE
+        vdocformatado := vdoc;
+    END IF;
 RETURN vdocformatado;
 END;
 /
@@ -41,9 +41,9 @@ SELECT nom_cliente, FORMATADOC(doc_cliente) DOCUMENTO FROM CLIENTE;
 CREATE OR REPLACE FUNCTION IDADE(vdtn IN CLIENTE.dtn_cliente%TYPE)
 RETURN NUMBER
 IS
-vidade NUMBER(3);
+    vidade NUMBER(3);
 BEGIN
-vidade := TRUNC(MONTHS_BETWEEN(SYSDATE, vdtn)/12);
+    vidade := TRUNC(MONTHS_BETWEEN(SYSDATE, vdtn)/12);
 RETURN vidade;
 END;
 /
